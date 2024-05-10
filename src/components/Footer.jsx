f30884fdfa281a10749df33678ef7d83318c1588
Josh-Manley <joshmanley45@gmail.com>
2024-05-09 23:43:48 -0600

    fixed some bugs and made the NavTabs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,16 @@
 import Link from 'react-router-dom';
 
-export default function Footer () {
+export default function Footer() {
   return (
     <ul className="nav">
       <li className="nav-item">
-        <Link>
-          to='https://github.com/Josh-Manley'
-        </Link>
+        <Link to="https://github.com/Josh-Manley"></Link>
         <img src="../assets/github-logo.png" alt="github icon" />
       </li>
-      <li className='nav-item'>
-        <Link>
-          to=''
-        </Link>
+      <li className="nav-item">
+        <Link to=""></Link>
         <img src="../assets/Linkedin-logo.png" alt="linkedin logo" />
       </li>
-      
     </ul>
-  )
+  );
 }
