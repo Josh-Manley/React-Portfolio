8b804f42c6059b51c590212aec87cc3e6cd174c5
Josh-Manley <joshmanley45@gmail.com>
2024-05-15 21:38:55 -0600

    adding resume page content

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,18 +2,28 @@ import Footer from '../components/Footer';
 
 export default function Resume() {
   return (
-    <div id='resume'>
+    <div id="resume">
       <h1>Resume</h1>
       <p>
         Download Resume <a href="">Link</a>
       </p>
       <h4>Front-End proficiencies</h4>
-      <ul>
-        <li></li>
+      <ul className='resume-list'>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Bootstap</li>
       </ul>
       <h4>Back-End proficiencies</h4>
-      <ul>
-        <li></li>
+      <ul className='resume-list'>
+        <li>APIs</li>
+        <li>Node</li>
+        <li>Express</li>
+        <li>MySQL, Sequelize</li>
+        <li>MongoDB, Mongoose</li>
+        <li>REST</li>
+        <li>GraphQL</li>
       </ul>
       <div>
         <Footer />
